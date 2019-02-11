f7103cf19356a0ddc1a0e21faba25f4922c4c5e1
Maria Firulyova <mariafirulevabio@gmail.com>
2019-02-11 23:43:45 +0300

    R function changed due to Seurat updating

--- R/pathways_annotation.R
+++ R/pathways_annotation.R
@@ -4,6 +4,7 @@
 #'
 #' @import Matrix
 #' @import Seurat
+#' @import progress
 #' @importFrom stats p.adjust phyper
 #'
 #' @param object is a Seurat object after scaling and clustering
@@ -17,18 +18,27 @@
 #' @export
 #'
 #' @examples pathways_annotation(pbmc, genes_list, 20, 500, 4, 10000)
-pathways_annotation <- function(object, genes_list, min_length, max_length, p_val_border, sample_number) {
-  data <- object@scale.data
+pathways_annotation <- function(object, genes_list, min_length=10, max_length=500,
+                                significance_level=0.05, sample_number=1000) {
+ 
+  data <- GetAssayData(object, slot = "scale.data")
+  N <- length(GetAssayData(object, slot = "data")@Dimnames[[2]])
+ 
   length_of_pathways <- lapply(genes_list, function(genes) {
-    length(which(rownames(data) %in% genes))
+    sum(genes %in% rownames(data))
   })
+ 
   genes_list <- genes_list[length_of_pathways >= min_length & length_of_pathways <= max_length]
   length_of_pathways <- length_of_pathways[length_of_pathways >= min_length & length_of_pathways <= max_length]
-  N <- length(object@data@Dimnames[[2]])
+ 
+  message(sprintf("%d pathways left after filtering", length(genes_list)))
+ 
   mat <- matrix(0L, nrow = length(genes_list), ncol = ncol(data))
+ 
   pb <- progress_bar$new(
-    format = "Create the reference matrix [:bar] :percent in :elapsed",
-    total = length(genes_list), clear = FALSE, width= 60)
+    format = "Creating the reference matrix [:bar] :percent in :elapsed",
+    total = length(genes_list), clear = FALSE, width=60, force=TRUE)
+ 
   for (i in 1:length(genes_list)){
     pb$tick()
     genes <- genes_list[[i]]
@@ -36,74 +46,25 @@ pathways_annotation <- function(object, genes_list, min_length, max_length, p_va
     genes_indexes <- genes_indexes - 1
     mat[i,] <- reference_matrix(data, genes_indexes)
   }
-  result_pvals <- matrix(nrow = length(genes_list), ncol = length(levels(object@ident)))
+  result_pvals <- matrix(nrow = length(genes_list), ncol = length(levels(Idents(object))))
+  message("Sampling starts")
   pval <- cumulative_matrix(data, unlist(length_of_pathways), mat, sample_number)
   pb <- progress_bar$new(
-    format = "Hypergeometric distribution [:bar] :percent in :elapsed",
-    total = nrow(pval) * length(levels(object@ident)), clear = FALSE, width= 60)
+    format = "Getting hypergeometric p-values [:bar] :percent in :elapsed",
+    total = nrow(pval) * length(levels(Idents(object))), clear = FALSE, width= 60)
   for (l in 1:nrow(pval)) {
-    K <- sum(-log10(pval[l,]) > p_val_border)
-    for (j in 1:length(levels(object@ident))){
+    K <- sum(pval[l, ] < significance_level)
+    for (j in 1:length(levels(Idents(object)))){
       pb$tick()
-      i <- levels(object@ident)[j]
-      indexes <- which(object@ident == i)
-      n <- sum(object@ident == i)
-      k <- sum(object@ident == i & -log10(pval[l, ]) > -log10(1/sample_number))
+      i <- levels(Idents(object))[j]
+      indexes <- which(Idents(object) == i)
+      n <- sum(Idents(object) == i)
+      k <- sum(Idents(object) == i & pval[l, ] < significance_level)
       result_pvals[l,j] <- phyper(k, K, N-K, n, lower.tail = FALSE)
     }
   }
   result_pvals[] <- p.adjust(result_pvals, method = 'bonferroni')
-  colnames(result_pvals) <- levels(object@ident)
-  rownames(result_pvals) <- names(genes_list)
-  return(result_pvals)
-}
-' Pathways annotation
-#' 
-#' @useDynLib pwannot
-#' 
-#' @import Matrix
-#' @import Seurat
-#' @importFrom stats p.adjust phyper
-#' 
-#' @param object is a Seurat object after scaling and clustering
-#' @param genes_list is a list of pathways which are included a pathway with a genes list, each of them contained the pathways genes in a character format (notice you should check whether genes of your organism capitalize or not)
-#' @param min_length is a minimal size (number of genes) of pathways which will be analyzed
-#' @param max_length is a maximal size (number of genes) of pathways which will be analyzed
-#' @param sample_number is a nubmer of random sample generations (10000 is recommended)
-#'
-#' @return the p-value matrix with pathways as rows and cell clusters as cols
-#' @export
-#' 
-#' @examples
-pathways_annotation <- function(object, genes_list, min_length, max_length, sample_number) {
-  data <- object@scale.data
-  length_of_pathways <- lapply(genes_list, function(genes) {
-    length(which(rownames(data) %in% genes))
-  })
-  genes_list <- genes_list[length_of_pathways >= min_length & length_of_pathways <= max_length]
-  length_of_pathways <- length_of_pathways[length_of_pathways >= min_length & length_of_pathways <= max_length]
-  N <- length(object@data@Dimnames[[2]])
-  mat <- matrix(0L, nrow = length(genes_list), ncol = ncol(data))
-  for (i in 1:length(genes_list)){
-    genes <- genes_list[[i]]
-    genes_indexes <- which(rownames(data) %in% genes)
-    genes_indexes <- genes_indexes - 1
-    mat[i,] <- reference_matrix(data, genes_indexes)
-  }
-  result_pvals <- matrix(nrow = length(genes_list), ncol = length(levels(object@ident)))
-  pval <- cumulative_matrix(data, unlist(length_of_pathways), mat, sample_number)
-  for (l in 1:nrow(pval)) {
-    K <- sum(-log10(pval[l,]) > -log10(1/sample_number))
-    for (j in 1:length(levels(object@ident))){
-      i <- levels(object@ident)[j]
-      indexes <- which(object@ident == i)
-      n <- sum(object@ident == i)
-      k <- sum(object@ident == i & -log10(pval[l, ]) > -log10(1/sample_number))
-      result_pvals[l,j] <- phyper(k, K, N-K, n, lower.tail = FALSE)
-    }
-  }
-  result_pvals[] <- p.adjust(result_pvals, method = 'bonferroni')
-  colnames(result_pvals) <- levels(object@ident)
+  colnames(result_pvals) <- levels(Idents(object))
   rownames(result_pvals) <- names(genes_list)
   return(result_pvals)
 }
--- R/plot_target_pw.R
+++ R/plot_target_pw.R
@@ -1,4 +1,35 @@
 #' Plot a target pathway distribution
+#'
+#' @useDynLib pwannot
+#'
+#' @import ggplot2
+#'
+#' @param object is a Seurat object after scaling and clustering
+#' @param annotation_result is your result of pathways_annotation function
+#' @param pw_name is a name of interested pathway
+#'
+#' @return the plot of a pathway distribution
+#' @export
+#'
+#' @examples plot_target__pw(pbmc, annotation_result, "KEGG_ALLOGRAFT_REJECTION")
+plot_target_pw <- function(object, gene_set, reduction="tsne") {
+  data <- GetAssayData(object, slot = "scale.data")
+  tsne <- object@reductions[[reduction]]@cell.embeddings
+  tsne <- as.data.frame(tsne)
+  colnames(tsne) <- paste0(reduction, 1:ncol(tsne))
+ 
+  genes_indexes <- which(rownames(data) %in% gene_set)
+  expression_signaling <- colMeans(data[genes_indexes,])
+  tsne$expression <- expression_signaling
+ 
+  ggplot(tsne, aes_string(x=paste0(reduction, 1),
+                          y=paste0(reduction, 2),
+                          color="expression"))+
+    geom_point()+
+    scale_color_gradient2(low='blue', high='red', mid = 'grey')+
+    theme_classic(base_size=8) +
+    theme(aspect.ratio = 1)
+}
 #' 
 #' @useDynLib pwannot
 #' 
@@ -29,3 +60,4 @@ plot_target_pw <- function(object, annotation_result, pw_name) {
     theme(aspect.ratio = 1)+
     ggtitle(pathway_name)
 }
+
